5f8f7a489fe1bf285345fef58ea0823c07f0e5c7
igoldsteinh <61714888+igoldsteinh@users.noreply.github.com>
2020-09-24 20:19:51 -0700

    prelim code for oc city plots

--- code/functions.R
+++ code/functions.R
@@ -7,90 +7,94 @@ library(ggtext)
 library(ckanr) # library to interact with CA Gov. Public Health Portal
 library(cowplot)
 
+
+# create watermarks -------------------------------------------------------
 watermark <- function(plt) ggdraw(plt) + draw_label("UC Irvine COVID Awareness Group", color = "#0064A4", alpha=0.4, size = 15, angle = 0, x=0.20, y=0.85, hjust = 0, vjust = 0)
 # Could also use stamp function
 # stamp(plot1, label = "DRAFT", color = "red")
 
-quiet <- function(x) { 
-  sink(tempfile()) 
-  on.exit(sink()) 
-  invisible(force(x)) 
-} 
+quiet <- function(x) {
+  sink(tempfile())
+  on.exit(sink())
+  invisible(force(x))
+}
 
+
+# county plots ------------------------------------------------------------
 get_county_plots <- function(counties_of_interest){
   # connect to CKAN instance
   ckanr_setup(url="https://data.ca.gov")
   ckan <- quiet(ckanr::src_ckan("https://data.ca.gov"))
-  
+
   # get resources
   resources <- rbind(resource_search("name:covid-19", as = "table")$results,
                      resource_search("name:hospitals by county", as = "table")$results)
-  
+
   resource_ids <- list(cases = resources$resource_id[resources$name == "COVID-19 Cases"],
                        tests = resources$resource_id[resources$name == "COVID-19 Testing"],
                        hosp = resources$resource_id[resources$name == "Hospitals By County"])
-  
+
   # pull resources into data frames (adds extra cols _id and _full_text)
   cases <- tbl(src = ckan$con, from = resource_ids$cases) %>%
     as_tibble( )%>%
-    select(-starts_with("_")) %>% 
+    select(-starts_with("_")) %>%
     mutate(date = as.Date(date))
-  
+
   tests <- tbl(src = ckan$con, from = resource_ids$test) %>%
     as_tibble() %>%
-    select(-starts_with("_")) %>% 
+    select(-starts_with("_")) %>%
     mutate(date = as.Date(date))
-  
+
   hosp <- tbl(src = ckan$con, from = resource_ids$hosp) %>%
     as_tibble() %>%
-    select(-starts_with("_")) %>% 
-    mutate(date = as.Date(todays_date)) %>% 
+    select(-starts_with("_")) %>%
+    mutate(date = as.Date(todays_date)) %>%
     select(-todays_date)
-  
+
   county_pop <- read_csv("data/county_pop.csv") %>% rename_all(str_to_lower)
-  
+
   # stay at home order start and end dates
   sah_start <- ymd("2020-03-19")
   sah_end <- ymd("2020-05-04")
-  
+
   per_n_people <- 1e6 # denominator for reporting counts (e.g, per million people)
   ma_n <- 7 # moving average window length in days
-  
+
   # Tidy Data
   hosp_tidy <- hosp %>%
-    filter(county %in% counties_of_interest) %>% 
-    pivot_longer(cols = -c(date, county)) %>% 
-    drop_na() %>% 
-    left_join(county_pop) %>% 
+    filter(county %in% counties_of_interest) %>%
+    pivot_longer(cols = -c(date, county)) %>%
+    drop_na() %>%
+    left_join(county_pop) %>%
     arrange(county, date)
-  
-  cases_tidy <- cases %>% 
-    filter(county %in% counties_of_interest) %>% 
-    pivot_longer(cols = -c(date, county)) %>% 
-    drop_na() %>% 
-    left_join(county_pop) %>% 
+
+  cases_tidy <- cases %>%
+    filter(county %in% counties_of_interest) %>%
+    pivot_longer(cols = -c(date, county)) %>%
+    drop_na() %>%
+    left_join(county_pop) %>%
     arrange(county, date)
-  
-  
+
+
   # Theme options
   theme_set(theme_bw(base_size = 12.5) +
               theme(legend.position = "bottom",
                     legend.title = element_text(size = 12)))
-  
+
   # keep color palette consistent across county sets
-  cbPalette <- c(Alameda = "#56B4E9", 
-                 `Los Angeles` = "#009E73", 
-                 Orange = "#E69F00", 
-                 `San Diego` = "#CC79A7", 
+  cbPalette <- c(Alameda = "#56B4E9",
+                 `Los Angeles` = "#009E73",
+                 Orange = "#E69F00",
+                 `San Diego` = "#CC79A7",
                  `Santa Clara` = "#0072B2",
                  Riverside = "#A857E6",
                  `San Bernardino` = "#1980E6",
                  Sacramento = "#CC625E",
                  `Contra Costa` = "#CAA1E6",
                  `San Francisco` = "#E6CA5C")
-  
+
   sah_alpha <- 0.2
-  
+
   gglayers = list(
     geom_line(size = 1.5),
     scale_color_manual(name = "County", values = cbPalette),
@@ -100,18 +104,18 @@ get_county_plots <- function(counties_of_interest){
                  expand = expansion(add=c(0,7))),
     scale_y_continuous(label = comma)
   )
-  
+
   # Hospitalizations
-  hospitalizations_plot_data <- hosp_tidy %>% 
-    filter(name == "hospitalized_covid_patients") %>% 
+  hospitalizations_plot_data <- hosp_tidy %>%
+    filter(name == "hospitalized_covid_patients") %>%
     group_by(county) %>%
-    mutate(value = runMean(value, ma_n)) %>% 
+    mutate(value = runMean(value, ma_n)) %>%
     drop_na()
-  
-  hospitalizations_plot <- hospitalizations_plot_data %>% 
+
+  hospitalizations_plot <- hospitalizations_plot_data %>%
     ggplot(aes(date, value / population * per_n_people, group = county, color = county)) +
     ggtitle(glue("Hospitalized Patients with COVID-19 <span style='font-size:14pt'>({ma_n} Day Moving Average)</span>")) +
-    #subtitle = glue("{ma_n} Day Moving Average")) 
+    #subtitle = glue("{ma_n} Day Moving Average"))
     theme(
       plot.title = element_markdown()
     ) +
@@ -123,18 +127,18 @@ get_county_plots <- function(counties_of_interest){
                             min(hospitalizations_plot_data$date)),
              xmax = sah_end, ymin = -Inf, ymax = Inf, alpha = sah_alpha) +
     annotate("text", y = 175, x = sah_end+8, label = "Stay at Home\nOrder Ended")
-  
+
   # ICU Occupancy
-  icu_plot_data <- hosp_tidy %>% 
-    filter(name %in% c("icu_covid_confirmed_patients", "icu_suspected_covid_patients")) %>% 
-    group_by(county, date, population) %>% 
-    summarize(value = sum(value)) %>% 
+  icu_plot_data <- hosp_tidy %>%
+    filter(name %in% c("icu_covid_confirmed_patients", "icu_suspected_covid_patients")) %>%
+    group_by(county, date, population) %>%
+    summarize(value = sum(value)) %>%
     group_by(county) %>%
     mutate(value = runMean(value, ma_n)) %>%
     drop_na()  %>%
-    filter(date >= min(hospitalizations_plot_data$date)) 
-  
-  icu_plot <- icu_plot_data %>% 
+    filter(date >= min(hospitalizations_plot_data$date))
+
+  icu_plot <- icu_plot_data %>%
     ggplot(aes(date, value / population * per_n_people, group = county, color = county)) +
     ggtitle(glue("ICU Patients with COVID-19 <span style='font-size:14pt'>({ma_n} Day Moving Average)</span>")) +
     #          subtitle = glue("{ma_n} Day Moving Average")) +
@@ -147,17 +151,17 @@ get_county_plots <- function(counties_of_interest){
              xmin = if_else(min(icu_plot_data$date) < sah_start, sah_start, min(icu_plot_data$date)),
              xmax = sah_end, ymin = -Inf, ymax = Inf, alpha = sah_alpha) +
     annotate("text", y = 51, x = sah_end+8, label = "Stay at Home\nOrder Ended")
-  
-  
+
+
   # Deaths
-  deaths_plot_data <- cases_tidy %>% 
-    filter(name == "newcountdeaths") %>% 
+  deaths_plot_data <- cases_tidy %>%
+    filter(name == "newcountdeaths") %>%
     group_by(county) %>%
-    mutate(value = runMean(value, ma_n)) %>% 
+    mutate(value = runMean(value, ma_n)) %>%
     drop_na() %>%
     filter(date >= min(hospitalizations_plot_data$date))
-  
-  deaths_plot <- deaths_plot_data %>% 
+
+  deaths_plot <- deaths_plot_data %>%
     ggplot(aes(date, value / population * per_n_people, group = county, color = county)) +
     ggtitle(glue(" New Daily Deaths due to COVID-19 <span style='font-size:14pt'>({ma_n} Day Moving Average)</span>")) +
     #          subtitle = glue("{ma_n} Day Moving Average")) +
@@ -172,16 +176,16 @@ get_county_plots <- function(counties_of_interest){
                             min(deaths_plot_data$date)),
              xmax = sah_end, ymin = -Inf, ymax = Inf, alpha = sah_alpha) +
     annotate("text", y = 3, x = sah_end+8, label = "Stay at Home\nOrder Ended")
-  
+
   # Cases
-  cases_plot_data <- cases_tidy %>% 
-    filter(name == "newcountconfirmed") %>% 
+  cases_plot_data <- cases_tidy %>%
+    filter(name == "newcountconfirmed") %>%
     group_by(county) %>%
-    mutate(value = runMean(value, ma_n)) %>% 
+    mutate(value = runMean(value, ma_n)) %>%
     drop_na() %>%
     filter(date >= min(hospitalizations_plot_data$date))
-  
-  cases_plot <- cases_plot_data %>% 
+
+  cases_plot <- cases_plot_data %>%
     ggplot(aes(date, value / population * per_n_people, group = county, color = county)) +
     ggtitle(glue("New Daily COVID-19 Cases <span style='font-size:14pt'>({ma_n} Day Moving Average)</span>")) +
     #          subtitle = glue("{ma_n} Day Moving Average")) +
@@ -196,10 +200,154 @@ get_county_plots <- function(counties_of_interest){
                             min(cases_plot_data$date)),
              xmax = sah_end, ymin = -Inf, ymax = Inf, alpha = sah_alpha) +
     annotate("text", y = 250, x = sah_end+8, label = "Stay at Home\nOrder Ended")
-  
-  return(list(hospitalizations_plot = hospitalizations_plot, 
-              icu_plot = icu_plot, 
-              cases_plot = cases_plot, 
+
+  return(list(hospitalizations_plot = hospitalizations_plot,
+              icu_plot = icu_plot,
+              cases_plot = cases_plot,
               deaths_plot = deaths_plot,
               data_date = max(hosp_tidy$date, cases_tidy$date)))
 }
+
+
+# oc city plots -----------------------------------------------------------
+get_city_plots <- function(cities_of_interest){
+
+  city_data <- read.csv("data/city_data.csv")
+
+  city_pop <- read_csv("data/city_pop.csv") %>% rename_all(str_to_lower)
+
+  # stay at home order start and end dates
+  sah_start <- ymd("2020-03-19")
+  sah_end <- ymd("2020-05-04")
+
+  per_n_people <- 1e5 # denominator for reporting counts (e.g, per million people)
+  ma_n <- 7 # moving average window length in days
+
+  # Tidy Data
+  tidy_city <- city_data%>%
+    left_join(city_pop)%>%
+    mutate(
+      positivity=new_cases/new_tests
+    )
+
+
+  # Theme options
+  theme_set(theme_bw(base_size = 12.5) +
+              theme(legend.position = "bottom",
+                    legend.title = element_text(size = 12)))
+
+  # keep color palette consistent across county sets
+
+  cbPalette <- c(Anaheim = "#56B4E9",
+                 `Santa Ana` = "#009E73",
+                  `Huntington Beach` = "#E69F00",
+                 `Irvine` = "#CC79A7",
+                 `Garden Grove` = "#0072B2")
+
+  sah_alpha <- 0.2
+
+  gglayers = list(
+    geom_line(size = 1.5),
+    scale_color_manual(name = "City", values = cbPalette),
+    scale_x_date(name = "Date",
+                 breaks = "14 day",
+                 date_labels = "%b %d",
+                 expand = expansion(add=c(0,7))),
+    scale_y_continuous(label = comma)
+  )
+
+  # Positivity
+  positive_plot_data <- tidy_city %>%
+    group_by(city) %>%
+    mutate(value = runMean(positivity, ma_n)) %>%
+    drop_na()
+
+  positive_plot <- positive_plot_data %>%
+    ggplot(aes(posted_date, value, group = city, color = city)) +
+    ggtitle(glue("Percent Positive COVID-19 Tests <span style='font-size:14pt'>({ma_n} Day Moving Average)</span>")) +
+    #subtitle = glue("{ma_n} Day Moving Average"))
+    theme(
+      plot.title = element_markdown()
+    ) +
+    gglayers +
+    ylab(glue("Percent Positive COVID-19 Tests")) +
+    annotate("rect",
+             xmin = if_else(min(tidy_city$posted_date) < sah_start,
+                            sah_start,
+                            min(tidy_city$posted_date)),
+             xmax = sah_end, ymin = -Inf, ymax = Inf, alpha = sah_alpha) +
+    annotate("text", y = 175, x = sah_end+8, label = "Stay at Home\nOrder Ended")
+
+  # Testing
+  testing_plot_data <- tidy_city %>%
+    group_by(city) %>%
+    mutate(value = runMean(new_tests, ma_n)) %>%
+    drop_na()
+
+  testing_plot <- testing_plot_data %>%
+    ggplot(aes(posted_date, value / population * per_n_people, group = city, color = city)) +
+    ggtitle(glue("Total COVID-19 Tests Peformed <span style='font-size:14pt'>({ma_n} Day Moving Average)</span>")) +
+    #          subtitle = glue("{ma_n} Day Moving Average")) +
+    theme(
+      plot.title = element_markdown()
+    ) +
+    gglayers +
+    ylab(glue("Total COVID-19 Tests Performed\nper {comma(per_n_people)} People")) +
+    annotate("rect",
+             xmin = if_else(min(testing_plot_data$posted_date) < sah_start, sah_start, min(testing_plot_data$posted_date)),
+             xmax = sah_end, ymin = -Inf, ymax = Inf, alpha = sah_alpha) +
+    annotate("text", y = 51, x = sah_end+8, label = "Stay at Home\nOrder Ended")
+
+
+  # Deaths
+  deaths_plot_data <- tidy_city %>%
+    group_by(city) %>%
+    mutate(value = runMean(new_deaths, ma_n)) %>%
+    drop_na()
+
+  deaths_plot <- deaths_plot_data %>%
+    ggplot(aes(posted_date, value / population * per_n_people, group = city, color = city)) +
+    ggtitle(glue(" New Daily Deaths due to COVID-19 <span style='font-size:14pt'>({ma_n} Day Moving Average)</span>")) +
+    #          subtitle = glue("{ma_n} Day Moving Average")) +
+    theme(
+      plot.title = element_markdown()
+    ) +
+    gglayers +
+    ylab(glue("New Daily Deaths due to COVID-19\nper {comma(per_n_people)} People")) +
+    annotate("rect",
+             xmin = if_else(min(deaths_plot_data$posted_date) < sah_start,
+                            sah_start,
+                            min(deaths_plot_data$posted_date)),
+             xmax = sah_end, ymin = -Inf, ymax = Inf, alpha = sah_alpha) +
+    annotate("text", y = 3, x = sah_end+8, label = "Stay at Home\nOrder Ended")
+
+  # Cases
+  cases_plot_data <- tidy_city %>%
+    group_by(city) %>%
+    mutate(value = runMean(new_cases, ma_n)) %>%
+    drop_na()
+
+  cases_plot <- cases_plot_data %>%
+    ggplot(aes(posted_date, value / population * per_n_people, group = city, color = city)) +
+    ggtitle(glue("New Daily COVID-19 Cases <span style='font-size:14pt'>({ma_n} Day Moving Average)</span>")) +
+    #          subtitle = glue("{ma_n} Day Moving Average")) +
+    theme(
+      plot.title = element_markdown()
+    ) +
+    gglayers +
+    ylab(glue("New Daily Confirmed COVID-19 Cases\nper {comma(per_n_people)} People")) +
+    annotate("rect",
+             xmin = if_else(min(cases_plot_data$posted_date) < sah_start,
+                            sah_start,
+                            min(cases_plot_data$posted_date)),
+             xmax = sah_end, ymin = -Inf, ymax = Inf, alpha = sah_alpha) +
+    annotate("text", y = 250, x = sah_end+8, label = "Stay at Home\nOrder Ended")
+
+  return(list(positivity_plot = positivity_plot,
+              testing_plot = testing_plot,
+              cases_plot = cases_plot,
+              deaths_plot = deaths_plot,
+              data_date = max(hosp_tidy$date, cases_tidy$date)))
+}
+
+
