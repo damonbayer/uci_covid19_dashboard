522636cd96428b345419b622312675a5cea7c095
markellekelly <markellekelly@gmail.com>
2021-05-11 12:11:01 -0700

    automatic update - 2021-05-11

--- docs/js/cases-map-gif.png.js
+++ docs/js/cases-map-gif.png.js
@@ -2,7 +2,7 @@
     $(document).ready(function() {
 	
 	$('#cases-map-gif_png').scianimator({
-	    'images': ['images/cases-map-gif.png1.png', 'images/cases-map-gif.png2.png', 'images/cases-map-gif.png3.png', 'images/cases-map-gif.png4.png', 'images/cases-map-gif.png5.png', 'images/cases-map-gif.png6.png', 'images/cases-map-gif.png7.png', 'images/cases-map-gif.png8.png', 'images/cases-map-gif.png9.png', 'images/cases-map-gif.png10.png', 'images/cases-map-gif.png11.png', 'images/cases-map-gif.png12.png', 'images/cases-map-gif.png13.png', 'images/cases-map-gif.png14.png'],
+	    'images': ['images/cases-map-gif.png1.png', 'images/cases-map-gif.png2.png', 'images/cases-map-gif.png3.png', 'images/cases-map-gif.png4.png', 'images/cases-map-gif.png5.png', 'images/cases-map-gif.png6.png', 'images/cases-map-gif.png7.png', 'images/cases-map-gif.png8.png', 'images/cases-map-gif.png9.png', 'images/cases-map-gif.png10.png', 'images/cases-map-gif.png11.png', 'images/cases-map-gif.png12.png', 'images/cases-map-gif.png13.png', 'images/cases-map-gif.png14.png', 'images/cases-map-gif.png15.png'],
 	    'width': 800,
 	    'delay': 1000,
 	    'loopMode': 'loop'
