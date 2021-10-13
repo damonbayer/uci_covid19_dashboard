5a7977de7a1b78dd76b8b96de158402e23eaeaff
vnminin <vminin@uci.edu>
2021-10-13 16:21:47 -0700

    rebuilt to update city trend plots

--- docs/js/tests-map-gif.png.js
+++ docs/js/tests-map-gif.png.js
@@ -2,7 +2,7 @@
     $(document).ready(function() {
 	
 	$('#tests-map-gif_png').scianimator({
-	    'images': ['images/tests-map-gif.png1.png', 'images/tests-map-gif.png2.png', 'images/tests-map-gif.png3.png', 'images/tests-map-gif.png4.png', 'images/tests-map-gif.png5.png', 'images/tests-map-gif.png6.png', 'images/tests-map-gif.png7.png', 'images/tests-map-gif.png8.png', 'images/tests-map-gif.png9.png', 'images/tests-map-gif.png10.png', 'images/tests-map-gif.png11.png', 'images/tests-map-gif.png12.png', 'images/tests-map-gif.png13.png', 'images/tests-map-gif.png14.png', 'images/tests-map-gif.png15.png', 'images/tests-map-gif.png16.png', 'images/tests-map-gif.png17.png', 'images/tests-map-gif.png18.png', 'images/tests-map-gif.png19.png'],
+	    'images': ['images/tests-map-gif.png1.png', 'images/tests-map-gif.png2.png', 'images/tests-map-gif.png3.png', 'images/tests-map-gif.png4.png', 'images/tests-map-gif.png5.png', 'images/tests-map-gif.png6.png', 'images/tests-map-gif.png7.png', 'images/tests-map-gif.png8.png', 'images/tests-map-gif.png9.png', 'images/tests-map-gif.png10.png', 'images/tests-map-gif.png11.png', 'images/tests-map-gif.png12.png', 'images/tests-map-gif.png13.png', 'images/tests-map-gif.png14.png', 'images/tests-map-gif.png15.png', 'images/tests-map-gif.png16.png', 'images/tests-map-gif.png17.png', 'images/tests-map-gif.png18.png', 'images/tests-map-gif.png19.png', 'images/tests-map-gif.png20.png'],
 	    'width': 800,
 	    'delay': 1000,
 	    'loopMode': 'loop'
