a31d469655f75f1213a6c0dd0d4c888d0c49091d
Damon Bayer <damon@damonbayer.com>
2021-12-09 11:48:49 -0800

    2021-12-09 Update

--- docs/js/pos-map-gif.png.js
+++ docs/js/pos-map-gif.png.js
@@ -2,7 +2,7 @@
     $(document).ready(function() {
 	
 	$('#pos-map-gif_png').scianimator({
-	    'images': ['images/pos-map-gif.png1.png', 'images/pos-map-gif.png2.png', 'images/pos-map-gif.png3.png', 'images/pos-map-gif.png4.png', 'images/pos-map-gif.png5.png', 'images/pos-map-gif.png6.png', 'images/pos-map-gif.png7.png', 'images/pos-map-gif.png8.png', 'images/pos-map-gif.png9.png', 'images/pos-map-gif.png10.png', 'images/pos-map-gif.png11.png', 'images/pos-map-gif.png12.png', 'images/pos-map-gif.png13.png', 'images/pos-map-gif.png14.png', 'images/pos-map-gif.png15.png', 'images/pos-map-gif.png16.png', 'images/pos-map-gif.png17.png', 'images/pos-map-gif.png18.png', 'images/pos-map-gif.png19.png', 'images/pos-map-gif.png20.png', 'images/pos-map-gif.png21.png'],
+	    'images': ['images/pos-map-gif.png1.png', 'images/pos-map-gif.png2.png', 'images/pos-map-gif.png3.png', 'images/pos-map-gif.png4.png', 'images/pos-map-gif.png5.png', 'images/pos-map-gif.png6.png', 'images/pos-map-gif.png7.png', 'images/pos-map-gif.png8.png', 'images/pos-map-gif.png9.png', 'images/pos-map-gif.png10.png', 'images/pos-map-gif.png11.png', 'images/pos-map-gif.png12.png', 'images/pos-map-gif.png13.png', 'images/pos-map-gif.png14.png', 'images/pos-map-gif.png15.png', 'images/pos-map-gif.png16.png', 'images/pos-map-gif.png17.png', 'images/pos-map-gif.png18.png', 'images/pos-map-gif.png19.png', 'images/pos-map-gif.png20.png', 'images/pos-map-gif.png21.png', 'images/pos-map-gif.png22.png'],
 	    'width': 800,
 	    'delay': 1000,
 	    'loopMode': 'loop'
